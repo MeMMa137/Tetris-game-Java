9d38f4df7d76d0eeaa1c43cd63b6c95c5890e449
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-22 16:46:24 +0200

    Tetromini spawn

--- Tetris/app/src/main/java/com/PD2/Tetris/App/Tetris.java
+++ Tetris/app/src/main/java/com/PD2/Tetris/App/Tetris.java
@@ -24,36 +24,36 @@ import java.awt.image.BufferedImage;
 import java.util.TimerTask;
 import java.util.Timer;
 
+public class Tetris extends JPanel implements KeyListener {
 
-
-public class Tetris extends JPanel implements  KeyListener{
     private Timer timer;
-    private final int delay = 500; 
+    private final int delay = 500;
     private boolean isPaused;
-    private boolean isOver=false;
+    private boolean isOver = false;
     private Tetromino currentTetromino;
-    private Tetromino nextTetromino; 
+    private Tetromino nextTetromino;
 
-    public static  Wall wall;
+    public static Wall wall;
 
     private Tetromino holdTetromino;
     private boolean holdUsed;
     //private Wall wall;
-    private JFrame gameFrame; 
-    private scoreEstimate scoreManager; 
-    Tetris(){
-        nextTetromino = Tetromino.random(); 
+    private JFrame gameFrame;
+    private scoreEstimate scoreManager;
+
+    Tetris() {
+        nextTetromino = Tetromino.random();
         //wall=new Wall();
-        gameFrame=new JFrame("NCKU Tetris");
-        timer=new Timer();
-        isPaused=false;
+        gameFrame = new JFrame("NCKU Tetris");
+        timer = new Timer();
+        isPaused = false;
         setFocusable(true);
-        scoreManager =new scoreEstimate();
+        scoreManager = new scoreEstimate();
         setFocusTraversalKeysEnabled(false);
-        currentTetromino=Tetromino.random();
+        currentTetromino = Tetromino.random();
         addKeyListener(this);
     }
-    
+
 // loading pictures
     public static BufferedImage I;
     public static BufferedImage J;
@@ -63,7 +63,6 @@ public class Tetris extends JPanel implements  KeyListener{
     public static BufferedImage T;
     public static BufferedImage Z;
     public static BufferedImage background;
-
 
     static {
         try {
@@ -75,11 +74,12 @@ public class Tetris extends JPanel implements  KeyListener{
             T = ImageIO.read(new File("img/T.png"));
             Z = ImageIO.read(new File("img/Z.png"));
             background = ImageIO.read(new File("img/background.png"));
-            wall=new Wall();
+            wall = new Wall();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void holdCurrentTetromino() {
         if (!holdUsed) {
             if (holdTetromino == null) {
@@ -94,3 +94,73 @@ public class Tetris extends JPanel implements  KeyListener{
             repaint();
         }
     }
+
+    public void endGame() {
+        // game over
+        System.out.println("Game Over");
+        gameFrame.dispose();
+
+        End_Menu end_menu = new End_Menu(scoreManager.getTotalScore());
+        end_menu.play_again_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("game start!!!");
+                end_menu.end_frame.dispose();
+                Tetris panel = new Tetris();  //start button new _frame
+                JFrame game_frame = panel.gameFrame;
+                game_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                //GameController gameController = new GameController(game_frame);
+                game_frame.setSize(810, 940);
+                game_frame.add(panel);
+                //game_frame.add(gameController);
+                game_frame.setVisible(true);
+                wall = new Wall();
+
+                panel.start();
+
+            }
+        });
+        end_menu.Menu_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                end_menu.end_frame.dispose();
+                Menu menu = new Menu(); //new 
+                menu.start_button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        System.out.println("game start!!!");
+                        menu.frame.dispose();
+                        Tetris panel = new Tetris();  //start button new
+                        //JFrame game_frame = new JFrame("NCKU Tetris");
+                        JFrame game_frame = panel.gameFrame;
+                        game_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                        //GameController gameController = new GameController(game_frame);
+                        game_frame.setSize(810, 940);
+                        game_frame.add(panel);
+                        //game_frame.add(gameController);
+                        game_frame.setVisible(true);
+                        wall = new Wall();
+                        panel.start();
+                    }
+                });
+            }
+        });
+        //isOver=true;
+        
+    public void spawnNewTetromino() {
+
+//        if (wall.add(currentTetromino) == Wall.LOSE) {
+//            currentTetromino=null;
+//            endGame();
+//        }
+//        else {
+        currentTetromino = nextTetromino;
+        nextTetromino = Tetromino.random();
+        repaint();
+//        }
+
+    }
+}
+}
