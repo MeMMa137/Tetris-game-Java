40f6b465d94d12e4399c2f3f62d231725bd37bc1
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-23 12:20:06 +0200

    Timer control

---
+++ Tetris/app/src/main/java/com/PD2/Tetris/App/scoreEstimate.java
@@ -0,0 +1,67 @@
+package com.PD2.Tetris.App;
+
+public class scoreEstimate {
+    
+    private int[] scoresPoolSpeed1 = {0, 100, 300, 600, 1000};
+    private int[] scoresPoolSpeed2 = {0, 200, 500, 800, 1500};
+    private int[] scoresPoolSpeed3 = {0, 300, 800, 1300, 2000};
+    private int scorePoolLength = scoresPoolSpeed1.length;
+
+    private int speed = 600;
+    private int totalScore = 0;
+    private int totalLine = 0;
+
+    private Timer gameTimer = new Timer();
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public int getTotalLine() {
+        return totalLine;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void resetScore() {
+        totalScore = 0;
+        totalLine = 0;
+    }
+    public void updateScore(int lines, int mode) {
+        //bonus
+        if(lines >= 1){
+            if((gameTimer.getTime() / 1000 < 10) && (totalScore > 0)){
+                totalScore = totalScore + (10-(int)(gameTimer.getTime()/1000))*60;
+            }
+            else if((gameTimer.getTime() / 1000 < 20) && (totalScore > 0)){
+                totalScore = totalScore + (20-(int)(gameTimer.getTime()/1000))*16;
+            }
+            else if((gameTimer.getTime() / 1000 < 30) && (totalScore > 0)){
+                totalScore = totalScore + (30-(int)(gameTimer.getTime()/1000))*5;
+            }
+            gameTimer.resetTimer();
+            gameTimer.startTimer();
+        }
+
+        if(mode == 0){
+            if (lines >= 0 && lines < scorePoolLength) {
+                if((totalScore < 500)){
+                    totalScore += scoresPoolSpeed1[lines];
+                    totalLine += lines;
+                    speed = 600-totalScore/10;
+                }
+                else if((totalScore >= 500 && totalScore < 2000)){
+                    totalScore += scoresPoolSpeed2[lines];
+                    totalLine += lines;
+                    speed = 550 - totalScore/10;
+                }
+                else if((totalScore >= 2000 && totalScore < 2500)){
+                    totalScore += scoresPoolSpeed3[lines];
+                    totalLine += lines;
+                    speed = 550 - totalScore/10;
+                    if(speed <= 250){
+                        speed = 250;
+                    }
+                }
